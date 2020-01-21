685dc6e8ac2d2a306c91c9e7234b903fba562afc
Maciej Wysocki <maciej.wysocki@gft.com>
2020-01-21 11:11:34 +0100

    Changed response message

--- Startup.cs
+++ Startup.cs
@@ -27,7 +27,7 @@ namespace azure_web_app_demo
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Web App Demo is here!");
             });
         }
     }
